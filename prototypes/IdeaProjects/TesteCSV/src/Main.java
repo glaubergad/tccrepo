d47b0c91250c2418e4594ae8a630d4a1100fb30c
Glauber Gadelha <glaubergad@gmail.com>
2019-07-10 13:01:29 -0300

    Adicionado FileChooser para testar os CSV

--- prototypes/IdeaProjects/TesteCSV/src/Main.java
+++ prototypes/IdeaProjects/TesteCSV/src/Main.java
@@ -1,36 +1,56 @@
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.List;
+
 import java.util.Map;
 
 
 public class Main {
-    private static final String SAMPLE_CSV_FILE_PATH = "C:\\Users\\glaubergad\\IdeaProjects\\TesteCSV\\src\\aposentadoria.csv";
+
     private static Dataset dataset;
 
     public static void main(String[] args) throws IOException {
         //metodo orientado a objetos
         System.out.println("Exibindo os cabeçalhos do CSV");
 
-        dataset = new Dataset(SAMPLE_CSV_FILE_PATH);
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Selecione o arquivo CSV: ");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Arquivos CSV", "csv");
+        jfc.addChoosableFileFilter(filter);
 
-        System.out.println(dataset.getCaminho());
+        int returnValue = jfc.showOpenDialog(null);
+        // int returnValue = jfc.showSaveDialog(null);
 
-        System.out.println("Usando o método headersToString");
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File  arquivoCSV = jfc.getSelectedFile();
+            System.out.println("Caminho obtido do JFileChooser :"+arquivoCSV.getAbsolutePath());
+            dataset = new Dataset(arquivoCSV.getAbsolutePath());
+        }else{
+            System.out.println("Arquivo invalido ou não encontrado");
+        }
+
+
+
+        System.out.println("Caminho obtido do objeto Dataset :"+dataset.getCaminho());
+
+        System.out.println("Usando o método headersToString()");
         dataset.headersToString();
 
-        System.out.println("Capturando o Map do headers");
+        System.out.println("\n Capturando o Map do headers");
         Map<String, Integer> headers = dataset.getHeaders();
 
+
+        System.out.println("\n Exibindo conteudo do Map de Headers");
         for (Map.Entry entry : headers.entrySet()) {
             System.out.println(entry.getKey() + ", " + entry.getValue());
         }
-        System.out.println("Quantidade de Colunas = " + headers.size());
+        System.out.println("\n Quantidade de Colunas = " + headers.size());
         int size = headers.size();
 
-        System.out.println("Exibindo as "+ (headers.size()-1) + " primeiras linhas do CSV");
+        System.out.println("\n Exibindo as 10 primeiras linhas do CSV com o método recordsToString()");
         dataset.recordsToString();
 
 
